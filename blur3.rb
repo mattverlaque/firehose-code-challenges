7f7a4e5a65d0557995ed6af00ad07b7dbc7f819e
Matt Verlaque <matt@runyourgym.com>
2016-12-10 01:27:59 -0500

    Manhattan Distance is a done deal

--- blur3.rb
+++ blur3.rb
@@ -15,20 +15,30 @@ class Image
     end
   end
 
-  def blur_image
+  def blur_image(md)
     copy = []                                 # Create empty secondary array
     array.each do |row|                       # Iterate over each array inside the master array
-      copy << row.clone                       # Create clone of each sub-array inside secondary array for clean reference
+      copy << row.clone                       # Create clone of each sub-array inside secondary array for current step reference
     end
-    array.each_with_index do |row, x|         # Iterate over each array inside the master array
-      row.each_with_index do |coord, y|       # Iterate over each element inside each sub-array
-        if copy[x][y] == 1                    # Check clean copy if coord == 0 or == 1
-          array[x+1][y] = 1 unless x+1 > 3    # Set adjacent coord to 1
-          array[x-1][y] = 1 unless x-1 < 0    # Set adjacent coord to 1
-          array[x][y+1] = 1 unless y+1 > 3    # Set adjacent coord to 1
-          array[x][y-1] = 1 unless y-1 < 0    # Set adjacent coord to 1
-        end
-      end                 
+
+    md_counter = 0                            # Init counter to compare against Manhattan Distance var
+
+    while md_counter < md do                    # Run while counter is less than Manhattan Distance var
+      array.each_with_index do |row, x|         # Iterate over each array inside the master array
+        row.each_with_index do |coord, y|       # Iterate over each element inside each sub-array
+          if copy[x][y] == 1                    # Check current step copy if coord == 0 or == 1
+            array[x+1][y] = 1 unless x+1 > array.count-1      # Set adjacent coord to 1
+            array[x-1][y] = 1 unless x-1 < 0                  # Set adjacent coord to 1
+            array[x][y+1] = 1 unless y+1 > array[0].count-1   # Set adjacent coord to 1
+            array[x][y-1] = 1 unless y-1 < 0                  # Set adjacent coord to 1
+          end
+        end                 
+      end
+      copy = []                                 # Re-set current step copy to blank array
+      array.each do |row|                       # Iterate over each array inside the master array
+        copy << row.clone                       # Create clone of each sub-array inside secondary array for current step reference
+      end
+      md_counter += 1                           # Increment counter to compare against Manhattan Distance var
     end
     return output_image
   end
@@ -36,10 +46,16 @@ class Image
 end
 
 image = Image.new([           # Create & assign new Image object
-  [0, 0, 0, 0],
-  [0, 1, 0, 0],
-  [0, 0, 0, 1],
-  [0, 0, 0, 0]
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 1, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0]
 ])
 
-image.blur_image
+image.blur_image(3)
